eae6961fb2f2159c9dbc170640aebbce7fa9e399
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-10-15 13:32:59 -0400

    added path varialble route

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 var express = require('express')
 var app = express();
+
+app.get('/', function(req, res){
+  res.send("Hi There You!")
+})
+
+app.get('/bye', function(req, res){
+  res.send("GOODBYE!!!")
+})
+
+app.get('/dog', function(req, res){
+  res.send("WOOF!!!")
+})
+
+app.get('/r/:something', function(req, res){
+  var thing = req.params.something
+  res.send("This is the " + thing)
+})
+
+app.get('*', function(req, res){
+  res.send("youareastar")
+})
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("Listening on Port 3000...");
+})
